54f119cf79280e12467c8bc3c2455b35b0fcaf94
eshwagadkar <eshwagadkar007@gmail.com>
2023-05-15 22:22:41 +0530

    updates

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from torch.functional import split
 from model_files.ml_predict import predict_plant, Network
 from pyfcm import FCMNotification
 import base64
-from decouple import config
+from settings import FCM_SERVER_KEY
+
 
 app = Flask("Plant Disease Detector")
 CORS(app)
-push_service = FCMNotification(api_key=config('API_KEY'))
+push_service = FCMNotification(api_key=FCM_SERVER_KEY)
 
 @app.route('/', methods=['POST'])
 def predict():
